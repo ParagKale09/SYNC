2fbaa1741b22d5e7d4c7ef4cdba098c3e766e950
Parag Kale <93380434+ParagKale09@users.noreply.github.com>
2023-02-20 01:01:01 +0530

    Update desktop_Notifier.py

--- Desktop_Notifier/desktop_Notifier.py
+++ Desktop_Notifier/desktop_Notifier.py
@@ -1,1 +1,9 @@
+from plyer import notification
+title = 'STUDY Alert....'
+message = 'Its time to study!!!'
 
+notification.notify(title=title,
+                    message=message,
+                    app_icon=None,
+                    timeout=10,
+                    toast=False)
