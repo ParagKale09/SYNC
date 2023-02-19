1da1fc01d613ea1d70c47ec6b458373c0f32fe88
Parag Kale <93380434+ParagKale09@users.noreply.github.com>
2023-02-20 01:01:59 +0530

    Update desktop_Notifier.py

--- Desktop_Notifier/desktop_Notifier.py
+++ Desktop_Notifier/desktop_Notifier.py
@@ -7,3 +7,14 @@ notification.notify(title=title,
                     app_icon=None,
                     timeout=10,
                     toast=False)
+
+"""
+from plyer import notification
+if __name__ == '__main__':
+        notification.notify(
+            title = "**Please Drink Water Now!!",
+            message ="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+            app_icon = "D:\KOKP\Internship\Projects\SYNC Python\Desktop Notifier\icon.ico",
+            timeout= 6
+            )
+"""
